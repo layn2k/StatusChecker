b1d746241aebb2a1f9ed08374b33b8c9dfc29406
layn2k <75184856+layn2k@users.noreply.github.com>
2025-10-03 16:18:14 +0300

    Added filter by date, mongodb, /status_info, cron
    Теперь берутся только записи в день запроса
    Подключена база данных mongodb
    Теперь бот умеет сокращать статусы до короткого текста (/status_info)
    Добавлена возможность ежедневной саммаризации при помощи cron

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
-from config import OLLAMA_URL, TELEGRAM_TOKEN, MODEL
+from config import OLLAMA_URL, TELEGRAM_TOKEN, MODEL, DB, CHAT_ID
 import telebot
 import ollama
-from datetime import datetime, date
-import time
 from prompts import EXTRACT_TITLE, SUMMARIZATION, STATUS
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from apscheduler.schedulers.background import BackgroundScheduler
+import pytz
+from datetime import datetime, date
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 daily_history = []
-
-daily_data = {
-    'status': {},
-    'report': {}
-}
+client = MongoClient(DB)
+db = client["bot"]
+collection = db["status_report"]
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -21,15 +22,19 @@ def start(message):
 def short_status(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, 'Собираю короткую сводку по статусам')
-    for status in daily_data['status']:
-        bot.reply_to(message, ollama.generate(model=MODEL, system=STATUS, think = False, prompt=status + daily_data['status'][status]).response)
+    statuses = [x for x in collection.find({"type": "status"}) if x.get("timestamp").date() == date.today()]
+    for status in statuses:
+        bot.reply_to(message, ollama.generate(model=MODEL, system=STATUS, think = False, prompt=status.get("project_name") + status.get("text")).response)
 
 @bot.message_handler(commands=['summary'])
 def summary(message):
+    scheduled_summary(message.chat.id)
+
+@bot.message_handler(commands=['clear'])
+def clear(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'Начинаю подведение итогов')
-    for status in daily_data['status']:
-        bot.reply_to(message, ollama.generate(model=MODEL, system = SUMMARIZATION, think = False, prompt=status + "\n" + daily_data['status'][status] + "\n" + "\n\n".join(daily_data['report'][status])).response)
+    collection.delete_many({})
+    bot.send_message(chat_id, 'База данных очищена')
 
 @bot.message_handler(content_types=['text'])
 def handle_message(message):
@@ -41,23 +46,45 @@ def handle_message(message):
 
 def process_status(text, message):
     project_name = extract_project_name(text)
-    if project_name not in daily_data['status']:
-        daily_data['status'][project_name] = str()
-    daily_data['status'][project_name]= text
+    if not(collection.find_one({"type": "status", "project_name":project_name})):
+        collection.update_one(
+            {
+                "type": "status",
+                "project_name": project_name,
+                "timestamp": datetime.fromtimestamp(message.date)
+            },
+            {
+                "$set": {
+                    "timestamp": datetime.fromtimestamp(message.date),
+                    "text": text
+                }
+            },
+            upsert=True
+        )
 
 def process_report(text, message):
     project_name = extract_project_name(text)
-    if not project_name:
-        bot.reply_to(message, "Укажите название проекта после #отчет")
-        return
-    elif project_name not in daily_data['status']:
-        bot.reply_to(message, f"Сначала создайте статус для проекта '{project_name}'")
-        return
-    else:
-        daily_data['report'][project_name] = []
-        daily_data['report'][project_name].append(text)
-
+    collection.insert_one({
+        "type":"report",
+        "project_name": project_name,
+        "timestamp": datetime.fromtimestamp(message.date),
+        "text": text
+    })
 def extract_project_name(text):
     return ollama.generate(model = MODEL, system = EXTRACT_TITLE, prompt=text, think=False, options={'temperature': 0.0, 'num_predict': 30}).response
 
+def scheduled_summary(chat_id = CHAT_ID):
+    bot.send_message(chat_id, 'Начинаю подведение итогов')
+    statuses = [x for x in collection.find({"type": "status"}) if x.get("timestamp").date() == date.today()]
+    reports = [x for x in collection.find({"type": "report"}) if x.get("timestamp").date() == date.today()]
+    for status in statuses:
+        bot.send_message(chat_id, ollama.generate(model=MODEL, system=SUMMARIZATION, think=False,
+                                              prompt=status.get("project_name") + "\n" + status.get("text") + "\n" +
+                                              "\n\n".join([x.get("text") for x in reports if x.get("project_name") == status.get( "project_name")])).response)
+scheduler = BackgroundScheduler(timezone=pytz.timezone("Europe/Moscow"))
+scheduler.add_job(scheduled_summary, 'cron', hour=12, minute=18)
+scheduler.start()
 bot.infinity_polling()
+
+
+
